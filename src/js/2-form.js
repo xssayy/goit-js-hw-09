33f1998ee488b9e1901121a5459dff470ffad82c
xssayy <nikiatanik@gmail.com>
2024-01-28 06:02:36 +0200

    finished

---
+++ src/js/2-form.js
@@ -0,0 +1,57 @@
+const form = document.querySelector('.feedback-form');
+const email = form.elements.email;
+email.classList.add('feedback-input');
+const message = form.elements.message;
+message.classList.add('feedback-input');
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', onInput);
+
+const LS_KEY = 'feedback-form-state';
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  const data = {
+    email,
+    message,
+  };
+
+  console.log(data);
+  localStorage.removeItem(LS_KEY, data);
+  form.reset();
+}
+
+function onInput() {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  const data = {
+    email,
+    message,
+  };
+  addToLS(LS_KEY, data);
+}
+
+function addToLS(key, value) {
+  const zip = JSON.stringify(value);
+  localStorage.setItem(key, zip);
+}
+
+function loadFromLS(key) {
+  const zip = localStorage.getItem(key);
+  try {
+    return JSON.parse(zip);
+  } catch {
+    return zip;
+  }
+}
+
+function checkout() {
+  const userData = loadFromLS(LS_KEY) || {};
+  form.elements.email.value = userData.email;
+  form.elements.message.value = userData.message;
+}
+
+checkout();
